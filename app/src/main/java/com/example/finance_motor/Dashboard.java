a757672cf8a0c0766232bc5fd29db3e4790aefb8
Charles <charlesadwin99@gmail.com>
2023-03-16 09:40:18 +0500

    f

--- app/src/main/java/com/example/finance_motor/Dashboard.java
+++ app/src/main/java/com/example/finance_motor/Dashboard.java
@@ -1,8 +1,23 @@
 package com.example.finance_motor;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import android.os.AsyncTask;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +25,72 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        // Initialize and execute the AsyncTask to fetch data from the PHP API
+        new GetDataTask().execute();
+    }
+
+    private class GetDataTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String result = "";
+            HttpURLConnection urlConnection = null;
+            try {
+                URL url = new URL("http://192.168.68.109/MVFP/dashboard.php");
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                result = stringBuilder.toString();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            // Parse the JSON data and display it in a table layout
+            try {
+                JSONArray jsonArray = new JSONArray(result);
+                TableLayout tableLayout = findViewById(R.id.table_layout);
+
+                // Create table rows and add data to them
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    TableRow tableRow = new TableRow(Dashboard.this);
+                    tableRow.setLayoutParams(new TableRow.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                    // Iterate through the JSON object and add each value to a table cell
+                    Iterator<String> keys = jsonObject.keys();
+                    while (keys.hasNext()) {
+                        String key = keys.next();
+                        TextView textView = new TextView(Dashboard.this);
+                        textView.setLayoutParams(new TableRow.LayoutParams(
+                                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                        textView.setPadding(16, 16, 16, 16);
+                        textView.setText(jsonObject.getString(key));
+                        tableRow.addView(textView);
+                    }
+                    tableLayout.addView(tableRow);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
